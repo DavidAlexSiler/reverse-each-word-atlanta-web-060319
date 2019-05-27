803d07f6c1c487e96f7a582f83140089aa972293
Alex Siler <davidalexsiler@gmail.com>
2019-05-27 18:19:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,9 +9,7 @@ end
   
 def reverse_each_word(s)
   arr = s.split(' ')
-  new_arr = []
-  arr.each do |word|
-    new_arr << word.reverse 
-  end
-  new_arr.join(' ')
+  arr.collect do |word|
+    word.reverse
+end
 end
